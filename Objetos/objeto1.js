29931d044fd787ae92afcb813ca17ecc05fd270b
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-06-23 16:01:11 +0000

    final

---
+++ Objetos/objeto1.js
@@ -0,0 +1,12 @@
+const desktop = {
+    Monitor: "Grande",
+    Mouse: "Cabeado",
+    GabineteCor: "Preto",
+    Click: function(){
+        console.log("Click realizado.")
+    },
+}
+
+console.log(desktop.GabineteCor)
+console.log(desktop.Monitor)
+console.log(desktop.Click)
